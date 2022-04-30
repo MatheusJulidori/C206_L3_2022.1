e7510c409db044073a849148a4b5460d97cc4654
Matheus Julidori <matheus_julidori@yahoo.com.br>
2022-04-29 21:14:44 -0300

    Exercicio 1 - Aula 11 Collections

--- Aula_10/src/main/java/br/inatel/cdg/Main.java
+++ Aula_10/src/main/java/br/inatel/cdg/Main.java
@@ -3,21 +3,63 @@ package br.inatel.cdg;
 import br.inatel.cdg.sistemaDeCompras.Carrinho;
 import br.inatel.cdg.sistemaDeCompras.Produto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Carrinho c = new Carrinho();
-        c.addProduto(new Produto("Playstation",4000));
-        c.addProduto(new Produto("Computador",5500));
-        c.addProduto(new Produto("Carro",45000));
-        c.addProduto(new Produto("Sapato",350));
 
+        int index;
+        Scanner sc = new Scanner(System.in);
+        List<Produto> listaDeProdutos = new ArrayList<>();
+        boolean flag = true;
+
+        listaDeProdutos.add(new Produto("Playstation",4000));
+        listaDeProdutos.add(new Produto("Computador",5500));
+        listaDeProdutos.add(new Produto("Carro",45000));
+        listaDeProdutos.add(new Produto("Sapato",350));
+
+        while(flag) {
+            System.out.println("Menu");
+            System.out.println("Entre com a opção desejada: ");
+            System.out.println("1 - Adicionar produto no carrinho");
+            System.out.println("2 - Ver lista de produtos");
+            System.out.println("3 - Sair");
+            index = sc.nextInt();
+            switch (index) {
+                case 1:
+                    System.out.println("Qual produto deseja adicionar?");
+                    int produto = sc.nextInt();
+                    c.addProduto(listaDeProdutos.get(produto - 1));
+                    break;
+                case 2:
+                    int count = 1;
+                    for (Produto p:listaDeProdutos){
+                        System.out.println(count+"-"+p.getNome());
+                        count++;
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Obrigado!");
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Opção invalida");
+                    break;
+            }
+        }
+
+        System.out.println("-----------Antes de ordenar--------------");
         c.mostrarProdutos();
-        System.out.println("-------------------------");
+        System.out.println("----------Ordenado crescente-------------");
         c.sort(1);
         c.mostrarProdutos();
-        System.out.println("-------------------------");
+        System.out.println("----------Ordenado descrescente----------");
         c.sort(-1);
         c.mostrarProdutos();
 
